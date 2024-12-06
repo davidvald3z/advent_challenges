c1c37b03d2ea60ba06a661f388a083a783983b3d
David Valdez <david_valdez@hotmail.com>
2024-12-06 16:45:04 -0600

    Radius challenge

---
+++ radius.rb
@@ -0,0 +1,31 @@
+class Radius
+    def program
+        loop do
+            puts "Escriba la palabra:"
+            word = gets.chomp
+
+            if palindromo(word)
+                puts "La palabra en orden inverso es : #{sort_pal(word)}"
+                break
+            end
+        end
+    end
+
+    def palindromo(pal)
+        is_pal = true
+        apl_arr = pal.split("")
+        last_pos = apl_arr.size - 1
+
+        apl_arr.each_with_index do |c, i|
+            is_pal = false if c != apl_arr[last_pos]
+            last_pos -=  1
+            break if last_pos < i
+        end
+
+        is_pal
+    end
+
+    def sort_pal(pal)
+        pal.split("").sort.reverse.join
+    end
+end
